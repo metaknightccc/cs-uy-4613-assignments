5c060b9e65f3b56a7e1a4de2b3851b8aee9b59bd
metaknightccc <sunyifanc@126.com>
2023-04-16 23:11:13 -0400

    complete

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -3,12 +3,11 @@ import torch
 import numpy as np
 from filterpy.kalman import KalmanFilter
 
-# Model
+# Use pre-trained Yolo V5 model
 model = torch.hub.load('ultralytics/yolov5', 'yolov5s', pretrained=True)
 
-# Video
-# file_name = 'ball.mp4'
-file_name = 'objectTracking_examples_multiObject.avi'
+file_name = 'ball.mp4'
+# file_name = 'objectTracking_examples_multiObject.avi'
 cap = cv2.VideoCapture(file_name)
 
 width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
@@ -20,33 +19,36 @@ fourcc = cv2.VideoWriter_fourcc(*'mp4v')
 out = cv2.VideoWriter('output_video.mp4', fourcc, fps, (width, height))
 
 
-
 # Initialize Kalman filter
 kf = KalmanFilter(dim_x=4, dim_z=2)
-kf.x = [0, 0, 0, 0]  # State vector: [x, y, dx, dy]
+# [x, y, dx, dy]
+kf.x = np.array([0, 0, 0, 0])
+# Transition matrix
 kf.F = np.array([[1, 0, 1, 0],
                  [0, 1, 0, 1],
                  [0, 0, 1, 0],
-                 [0, 0, 0, 1]])  # State transition matrix
+                 [0, 0, 0, 1]])
+# Observation matrix
 kf.H = np.array([[1, 0, 0, 0],
-                 [0, 1, 0, 0]])  # Observation matrix
-kf.R *= 10  # Measurement noise covariance
-kf.Q *= 0.1  # Process noise covariance
-kf.P *= 100  # State uncertainty covariance
+                 [0, 1, 0, 0]])
 
+kf.R *= 10
+kf.Q *= 0.1
+kf.P *= 100
 
+initial_detect = False
+
+trajectory_arr = []
 while True:
     ret, frame = cap.read()
 
     if not ret:
         break
 
-    # Inference
     results = model(frame)
 
-    # Draw bounding boxes and labels on the frame
     for *xyxy, conf, cls in results.xyxy[0].tolist():
-        # detect ball only
+        # Detect ball only, ball id=32
         if cls == 32:
             detected_ball = True
 
@@ -58,19 +60,28 @@ while True:
             centroid_y = int((xyxy[1] + xyxy[3]) / 2)
             frame = cv2.circle(frame, (centroid_x, centroid_y), 5, (0, 255, 0), -1)
 
-        if detected_ball:
-            # Predict the next centroid position using the Kalman filter
-            kf.predict()
-            predicted_centroid_x, predicted_centroid_y = int(kf.x[0]), int(kf.x[1])
+            # Use the ball's position as the beginning value of Kalman filter
+            if initial_detect is False:
+                initial_detect = True
+                kf.x = np.array([centroid_x, centroid_y, 0, 0])
+                trajectory_arr.append([centroid_x, centroid_y])
 
-            # Draw the predicted centroid
-            frame = cv2.circle(frame, (predicted_centroid_x, predicted_centroid_y), 5, (0, 255, 255), -1)
-    # Display the frame
+            kf.update(np.array([centroid_x, centroid_y]))
+
+    if detected_ball:
+        # Predict
+        kf.predict()
+        frame = cv2.circle(frame, (int(kf.x[0]), int(kf.x[1])), 5, (0, 0, 255), -1)
+        trajectory_arr.append([int(kf.x[0]), int(kf.x[1])])
+
+    # Draw trajectory
+    for i in range(len(trajectory_arr)-1):
+        frame = cv2.line(frame, (trajectory_arr[i][0], trajectory_arr[i][1]),
+                         (trajectory_arr[i+1][0], trajectory_arr[i+1][1]), (0, 0, 0), 3)
+
     cv2.imshow('Video', frame)
-
     out.write(frame)
 
-    # Exit the loop if the user presses the 'q' key
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
